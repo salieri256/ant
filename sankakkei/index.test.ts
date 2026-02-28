c75c01248a9db3b368759028ad7e49add6ce007e
salieri256 <nakakou201250@gmail.com>
2026-02-28 14:09:06 +0900

    test: テストメッセージに問題名を追加

--- sankakkei/index.test.ts
+++ sankakkei/index.test.ts
@@ -3,12 +3,12 @@ import { test } from 'node:test'
 import { solve as n3 } from './n3.ts'
 import { solve as nlogn } from './nlogn.ts'
 
-test('O(n^3)の実装が正しく動作すること', () => {
+test('「三角形」のO(n^3)の実装が正しく動作すること', () => {
   assert.deepStrictEqual(n3(5, [2,3,4,5,10]), 12)
   assert.deepStrictEqual(n3(4, [4,5,10,20]), 0)
 })
 
-test('O(n log n)の実装が正しく動作すること', () => {
+test('「三角形」のO(n log n)の実装が正しく動作すること', () => {
   assert.deepStrictEqual(nlogn(5, [2,3,4,5,10]), 12)
   assert.deepStrictEqual(nlogn(4, [4,5,10,20]), 0)
 })
